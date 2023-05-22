1613faf560c38c9e354f98e41046b12d48845831
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-22 20:38:01 +0300

    Add files via upload

---
+++ src/Worker.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Worker {
+    public List<Integer> mymap(List<Integer> numbers) {
+        // Sort the numbers using any sorting algorithm
+        bubbleSort(numbers);
+        return numbers;
+    }
+
+    private void bubbleSort(List<Integer> numbers) {
+        int n = numbers.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (numbers.get(j) > numbers.get(j + 1)) {
+                    int temp = numbers.get(j);
+                    numbers.set(j, numbers.get(j + 1));
+                    numbers.set(j + 1, temp);
+                }
+            }
+        }
+    }
+}
