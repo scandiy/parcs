c2827b0e7fe5ed2857b76d5f3a70e115b7545b34
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-23 12:10:05 +0300

    Update Solver.java

--- src/Solver.java
+++ src/Solver.java
@@ -122,35 +122,3 @@ public class Solver implements AM {
     }
 }
 
-class WorkerTask implements AM {
-
-    public void run(AMInfo info) {
-        List<Integer> chunk = (List<Integer>) info.parent().readObject();
-        Worker worker = (Worker) info.parent().readObject();
-
-        List<Integer> sortedChunk = worker.mymap(chunk);
-
-        info.parent().write(sortedChunk);
-    }
-}
-
-class Worker implements Serializable {
-    public List<Integer> mymap(List<Integer> array) {
-        // Sorting algorithm (e.g., bubble sort)
-        bubbleSort(array);
-        return array;
-    }
-
-    private void bubbleSort(List<Integer> numbers) {
-        int n = numbers.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (numbers.get(j) > numbers.get(j + 1)) {
-                    int temp = numbers.get(j);
-                    numbers.set(j, numbers.get(j + 1));
-                    numbers.set(j + 1, temp);
-                }
-            }
-        }
-    }
-}
