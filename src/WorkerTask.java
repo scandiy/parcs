63d5843c203abc7a7094d873cac075f8effdbf02
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-23 12:05:15 +0300

    Add files via upload

---
+++ src/WorkerTask.java
@@ -0,0 +1,16 @@
+import parcs.AM;
+import parcs.AMInfo;
+import parcs.channel;
+
+import java.util.List;
+
+public class WorkerTask implements AM {
+    public void run(AMInfo info) {
+        List<Integer> chunk = (List<Integer>) info.parent().readObject();
+        Worker worker = (Worker) info.parent().readObject();
+
+        List<Integer> sortedChunk = worker.mymap(chunk);
+
+        info.parent().write(sortedChunk);
+    }
+}
