bf1b2e6b3c8765b0c26a14b0f944521053fbd37c
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-23 12:20:41 +0300

    Update WorkerTask.java

--- src/WorkerTask.java
+++ src/WorkerTask.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 public class WorkerTask implements AM {
     public void run(AMInfo info) {
-        List<Integer> chunk = (List<Integer>) info.parent_read().readObject();
-        Worker worker = (Worker) info.parent_read().readObject();
+        List<Integer> chunk = (List<Integer>) info.parent().readObject();
+        Worker worker = (Worker) info.parent().readObject();
         List<Integer> sortedChunk = worker.mymap(chunk);
 
-        info.parent_write().write(sortedChunk);
+        info.parent().write(sortedChunk);
     }
 }
