4123791ecfd248acd29d38f7012e8483d5f7a5d7
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-24 14:20:48 +0300

    Update BubbleSort.java

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,28 +4,26 @@ import java.util.List;
 import parcs.*;
 
 public class BubbleSort implements AM {
+    
     public void run(AMInfo info) {
-        int number = info.parent.readInt();
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(number);
+        int[] numbers = (int[]) info.parent.readObject();
 
-        System.out.println("Received number: " + number);
+        System.out.println("Received array: " + Arrays.toString(numbers));
 
-        // Sort the number using bubble sort
-        for (int i = 0; i < numbers.size() - 1; i++) {
-            for (int j = 0; j < numbers.size() - i - 1; j++) {
-                if (numbers.get(j) > numbers.get(j + 1)) {
-                    int temp = numbers.get(j);
-                    numbers.set(j, numbers.get(j + 1));
-                    numbers.set(j + 1, temp);
+        // Sort the array using bubble sort
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
                 }
             }
         }
 
-        int sortedNumber = numbers.get(0);
-        System.out.println("Sorted number: " + sortedNumber);
+        System.out.println("Sorted array: " + Arrays.toString(numbers));
 
-        // Send the sorted number back to the parent point
-        info.parent.write(sortedNumber);
-    }
+        // Send the sorted array back to the parent point
+        info.parent.write(numbers);
+}
 }
