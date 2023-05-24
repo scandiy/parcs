5f1621a7e4139c8450e4035fe0cef36e7e40ab07
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-24 13:56:41 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main {
         List<point> points = new ArrayList<>();
         List<channel> channels = new ArrayList<>();
 
-        for (int i = 0; i < info.cnt; i++) {
+        for (int i = 0; i < info.cores; i++) {
             point p = info.createPoint();
             points.add(p);
             channels.add(p.createChannel());
@@ -33,7 +33,7 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             int value = arr[i];
-            channel c = channels.get(i % info.cnt);
+            channel c = channels.get(i % info.cores);
             c.write(value);
         }
 
@@ -43,7 +43,7 @@ public class Main {
 
         List<Integer> sortedNumbers = new ArrayList<>();
 
-        for (int i = 0; i < info.cnt; i++) {
+        for (int i = 0; i < info.cores; i++) {
             channel c = points.get(i).createChannel();
             sortedNumbers.add(c.readInt());
             c.close();
