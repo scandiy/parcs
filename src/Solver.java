f3e69a37eec01fee3577dd2aa11b2dd3eee2b5ad
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-24 10:54:28 +0300

    Update Solver.java

--- src/Solver.java
+++ src/Solver.java
@@ -1,5 +1,8 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
 import parcs.*;
 
 public class Solver implements AM {
@@ -23,39 +26,48 @@ public class Solver implements AM {
         int workers = info.getJob().getN();
         int chunkSize = (n - 1) / workers + 1;
         boolean sorted = false;
-        while (!sorted) {
-            sorted = true;
-            channel[] channels = new channel[workers];
-            point[] points = new point[workers];
-            for (int i = 0; i < workers; i++) {
-                int start = i * chunkSize;
-                int end = Math.min(start + chunkSize, n);
-                int[] subArray = Arrays.copyOfRange(array, start, end);
 
-                point p = info.createPoint();
-                channel c = p.createChannel();
-                points[i] = p;
-                channels[i] = c;
-                points[i].execute("BubbleSort");
-                c.write(subArray);
+        point[] points = new point[workers];
+        channel[] channels = new channel[workers];
+
+        for (int i = 0; i < workers; i++) {
+            point p = info.createPoint();
+            channel c = p.createChannel();
+            points[i] = p;
+            channels[i] = c;
+            p.execute("BubbleSort");
+            c.write(array);
+            c.write(i * chunkSize);
+            c.write(Math.min((i + 1) * chunkSize - 1, n - 1));
+        }
+
+        for (channel channel : channels) {
+            int[] sortedChunk = (int[]) channel.readObject();
+            if (sortedChunk == null) {
+                System.out.println("Error: Sorting failed. Received null chunk.");
+                return;
             }
+            if (sortedChunk.length > 0) {
+                sorted = false;
+                int startIndex = (int) channel.readObject();
+                System.arraycopy(sortedChunk, 0, array, startIndex, sortedChunk.length);
+            }
+        }
 
-            for (channel channel : channels) {
-                int[] sortedChunk = (int[]) channel.readObject();
-                if (sortedChunk == null) {
-                    System.out.println("Error: Sorting failed. Received null chunk.");
-                    return;
-                }
-                if (sortedChunk.length > 0) {
+        if (!sorted) {
+            sorted = true;
+            for (int i = 0; i < n - 1; i++) {
+                if (array[i] > array[i + 1]) {
                     sorted = false;
-                    int startIndex = (int) channel.readObject();
-                    System.arraycopy(sortedChunk, 0, array, startIndex, sortedChunk.length);
+                    break;
                 }
             }
         }
 
         // Write sorted array to output file
         writeArrayToFile(array, "sorted.txt");
+
+        System.out.println("Sorting " + (sorted ? "succeeded" : "failed"));
     }
 
     private int[] generateRandomArray(int n, int min, int max) {
