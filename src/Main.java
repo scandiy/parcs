d07d730ff51999ed83ec87e90ba06018989bbc88
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-24 13:33:59 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,36 +12,95 @@ public class Main {
 
         AMInfo info = new AMInfo(curtask, null);
 
-        Scanner sc = new Scanner(new File(curtask.findFile("input")));
+        Scanner sc = new Scanner(new File(curtask.findFile("input.txt")));
         int n = sc.nextInt();
         int[] arr = new int[n];
-        
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        long startTime = System.nanoTime();
+        int numPoints = 4; // Number of points (daemons) to use
+        int batchSize = n / numPoints; // Number of elements to assign to each point
 
+        List<point> points = new ArrayList<>();
         List<channel> channels = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
+        // Create points and channels
+        for (int i = 0; i < numPoints; i++) {
+            point p = info.createPoint();
+            channel c = p.createChannel();
+            points.add(p);
+            channels.add(c);
+        }
+
+        // Assign numbers to points
+        int startIndex = 0;
+        int endIndex = batchSize - 1;
+
+        for (int i = 0; i < numPoints; i++) {
+            point p = points.get(i);
+            channel c = channels.get(i);
+
+            // Send numbers to the point
+            for (int j = startIndex; j <= endIndex; j++) {
+                c.write(arr[j]);
+            }
+
+            // Notify the point that there are no more numbers
+            c.write(-1);
+
+            // Update the start and end indices for the next point
+            startIndex = endIndex + 1;
+            endIndex = startIndex + batchSize - 1;
+        }
+
+        // Wait for the points to finish
+        for (int i = 0; i < numPoints; i++) {
+            points.get(i).execute("BubbleSort");
+        }
+
+        // Collect the sorted numbers from each point
+        List<Integer> sortedNumbers = new ArrayList<>();
+
+        for (int i = 0; i < numPoints; i++) {
+            point p = points.get(i);
+            channel c = channels.get(i);
+
+            while (true) {
+                int num = c.readInt();
+                if (num == -1) {
+                    break; // Reached the end of numbers from this point
+                }
+                sortedNumbers.add(num);
+            }
+
+            p.delete();
+            c.close();
+        }
+
+        // Merge and print the sorted numbers
+        int[] mergedSortedNumbers = new int[sortedNumbers.size()];
+        for (int i = 0; i < sortedNumbers.size(); i++) {
+            mergedSortedNumbers[i] = sortedNumbers.get(i);
+        }
+
+        // Perform final sorting on the merged numbers
+        for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                if (mergedSortedNumbers[j] > mergedSortedNumbers[j + 1]) {
+                    int temp = mergedSortedNumbers[j];
+                    mergedSortedNumbers[j] = mergedSortedNumbers[j + 1];
+                    mergedSortedNumbers[j + 1] = temp;
                 }
             }
         }
 
         System.out.println("Sorted array:");
-        for (int num : arr) {
+        for (int num : mergedSortedNumbers) {
             System.out.print(num + " ");
         }
         System.out.println();
-
-        double totalTime = (double) (System.nanoTime() - startTime) / 1000000000;
-        System.out.println("Execution time: " + totalTime + " seconds");
 
         curtask.end();
     }
