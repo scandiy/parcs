2a78547bb2a130955fa562e76978a1621165366d
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-24 14:24:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,27 +15,35 @@ public class Main {
         Scanner sc = new Scanner(new File(curtask.findFile("input")));
         int n = sc.nextInt();
         int[] arr = new int[n];
-        
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
         long startTime = System.nanoTime();
 
-        List<channel> channels = new ArrayList<>();
+        point p = info.createPoint();
+        channel c = p.createChannel();
+        p.execute("BubbleSort");
+        c.write(arr);
+        channel resultChannel = p.createChannel();
+        p.getMonitor().addChannel(resultChannel);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+        p.execute("BubbleSort");
+        c.write(-1); // Signal the end of input numbers
+
+        List<Integer> sortedNumbers = new ArrayList<>();
+
+        while (true) {
+            int sortedNum = resultChannel.readInt();
+            if (sortedNum == -1) {
+                break; // Exit the loop when all numbers are received
             }
+            sortedNumbers.add(sortedNum);
         }
 
         System.out.println("Sorted array:");
-        for (int num : arr) {
+        for (int num : sortedNumbers) {
             System.out.print(num + " ");
         }
         System.out.println();
