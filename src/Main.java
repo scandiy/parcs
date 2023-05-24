1feb0426a1cdeba3aeed3facddece9d1e2c9eba0
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-24 14:05:23 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,41 +15,29 @@ public class Main {
         Scanner sc = new Scanner(new File(curtask.findFile("input")));
         int n = sc.nextInt();
         int[] arr = new int[n];
-
+        
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
         long startTime = System.nanoTime();
 
-        List<point> points = new ArrayList<>();
         List<channel> channels = new ArrayList<>();
 
-        for (int i = 0; i < info.hosts().size(); i++) {
-            point p = info.createPoint();
-            points.add(p);
-            channels.add(p.createChannel());
-        }
-
         for (int i = 0; i < n; i++) {
-            int value = arr[i];
-            channel c = channels.get(i % info.hosts().size());
-            c.write(value);
-        }
-
-        for (channel c : channels) {
-            c.close();
+            point p = info.createPoint();
+            channel c = p.createChannel();
+            p.execute("BubbleSort");
+            c.write(arr[i]);
+            channels.add(c);
         }
 
         List<Integer> sortedNumbers = new ArrayList<>();
 
-        for (int i = 0; i < info.hosts().size(); i++) {
-            channel c = points.get(i).createChannel();
+        for (channel c : channels) {
             sortedNumbers.add(c.readInt());
-            c.close();
         }
 
-        System.out.println("Received " + sortedNumbers.size() + " numbers. Sorting completed.");
         System.out.println("Sorted array:");
         for (int num : sortedNumbers) {
             System.out.print(num + " ");
