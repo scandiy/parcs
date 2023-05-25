e46c9a6d2a80967197e589a809210f10c3c315b8
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-25 12:02:03 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,141 +1,93 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.io.File;
+import java.io.*;
+import java.util.*;
 
 import parcs.*;
 
-public class Main implements AM {
+public class Solver implements AM {
+    public static void main(String[] args) {
+        System.out.print("class Solver start method main\n");
 
-    public static void main(String[] args) throws Exception {
-        task curtask = new task();
-        curtask.addJarFile("Main.jar");
-        curtask.addJarFile("BubbleSort.jar");
+        task mainTask = new task();
 
-        AMInfo info = new AMInfo(curtask, null);
+        mainTask.addJarFile("Solver.jar");
+        mainTask.addJarFile("Count.jar");
 
-        Scanner sc = new Scanner(new File(curtask.findFile("input.txt")));
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+        System.out.print("class Solver method main adder jars\n");
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+        (new Solver()).run(new AMInfo(mainTask, (channel) null));
 
-        long startTime = System.nanoTime();
+        System.out.print("class Solver method main finish work\n");
 
-        List<channel> channels = new ArrayList<>();
-
-        int numDaemons = 2;
-        int chunkSize = n / numDaemons;
-
-        for (int i = 0; i < numDaemons; i++) {
-            int start = i * chunkSize;
-            int end = (i == numDaemons - 1) ? n : (i + 1) * chunkSize;
-
-            int[] partition = new int[end - start];
-            System.arraycopy(arr, start, partition, 0, end - start);
-
-            point newPoint = info.createPoint();
-            channel newChannel = newPoint.createChannel();
-
-            channels.add(newChannel);
-
-            newChannel.write(partition);
-            newChannel.write(end - start);
-
-            newPoint.execute("BubbleSort.jar");
-        }
-
-        List<Integer> sortedList = new ArrayList<>();
-        for (channel channel : channels) {
-            int[] sortedPartition = (int[]) channel.readObject();
-            for (int num : sortedPartition) {
-                sortedList.add(num);
-            }
-        }
-
-        int[] sortedArray = new int[n];
-        for (int i = 0; i < n; i++) {
-            sortedArray[i] = sortedList.get(i);
-        }
-
-        mergeSort(sortedArray);
-
-        System.out.println("Sorted array:");
-        for (int num : sortedArray) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-
-        double totalTime = (double) (System.nanoTime() - startTime) / 1000000000;
-        System.out.println("Execution time: " + totalTime + " seconds");
-
-        curtask.end();
-    }
-
-    private static void mergeSort(int[] arr) {
-        if (arr.length > 1) {
-            int mid = arr.length / 2;
-            int[] left = new int[mid];
-            int[] right = new int[arr.length - mid];
-
-            System.arraycopy(arr, 0, left, 0, mid);
-            System.arraycopy(arr, mid, right, 0, arr.length - mid);
-
-            mergeSort(left);
-            mergeSort(right);
-
-            merge(arr, left, right);
-        }
-    }
-
-    private static void merge(int[] arr, int[] left, int[] right) {
-        int i = 0, j = 0, k = 0;
-
-        while (i < left.length && j < right.length) {
-            if (left[i] <= right[j]) {
-                arr[k] = left[i];
-                i++;
-            } else {
-                arr[k] = right[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < left.length) {
-            arr[k] = left[i];
-            i++;
-            k++;
-        }
-
-        while (j < right.length) {
-            arr[k] = right[j];
-            j++;
-            k++;
-        }
+        mainTask.end();
     }
 
     public void run(AMInfo info) {
-        channel channel = info.parent.readChannel();
-        int[] partition = (int[]) channel.readObject();
-        int length = channel.readInt();
+        long n, a, b;
 
-        bubbleSort(partition, length);
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(info.curtask.findFile("input.txt")));
 
-        channel.write(partition);
+            n = new Long(in.readLine()).longValue();
+            a = new Long(in.readLine()).longValue();
+            b = new Long(in.readLine()).longValue();
+        } catch (IOException e) {
+            System.out.print("Error while reading input\n");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.print("class Solver method run read data from file\n");
+
+        long[] array = generateArray(n, a, b);
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        bubbleSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
     }
 
-    private void bubbleSort(int[] arr, int length) {
-        for (int i = 0; i < length - 1; i++) {
-            for (int j = 0; j < length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+    static public long[] generateArray(long n, long a, long b) {
+        long[] array = new long[(int) n];
+        Random rand = new Random();
+
+        for (int i = 0; i < n; i++) {
+            array[i] = rand.nextLong() % (b - a + 1) + a;
+        }
+
+        return array;
+    }
+
+    static public void bubbleSort(long[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Swap elements
+                    long temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
                 }
             }
+
+            // If no two elements were swapped, the array is already sorted
+            if (!swapped) {
+                break;
+            }
         }
+    }
+
+    static public void printArray(long[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
     }
 }
