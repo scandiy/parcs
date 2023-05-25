8103c990590cc5d6dcce7c9a2a0a0531ea35cf91
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-25 11:21:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,17 @@ import java.io.File;
 
 import parcs.*;
 
-public class Main {
+public class Main implements AM {
+    private static final int CHUNK_SIZE = 1000;
+
     public static void main(String[] args) throws Exception {
         task curtask = new task();
+        curtask.addJarFile("Main.jar");
         curtask.addJarFile("BubbleSort.jar");
 
         AMInfo info = new AMInfo(curtask, null);
 
-        Scanner sc = new Scanner(new File(curtask.findFile("input")));
+        Scanner sc = new Scanner(new File(curtask.findFile("input.txt")));
         int n = sc.nextInt();
         int[] arr = new int[n];
 
@@ -24,34 +27,27 @@ public class Main {
 
         List<channel> channels = new ArrayList<>();
 
-        // Define the number of daemons
-        int numDaemons = 2; // Adjust the number of daemons as needed
-
-        // Calculate the partition size for each daemon
-        int partitionSize = n / numDaemons;
+        int numDaemons = info.cores;
+        int chunkSize = n / numDaemons;
 
         for (int i = 0; i < numDaemons; i++) {
-            int start = i * partitionSize;
-            int end = (i == numDaemons - 1) ? n : (i + 1) * partitionSize;
+            int start = i * chunkSize;
+            int end = (i == numDaemons - 1) ? n : (i + 1) * chunkSize;
 
-            // Create a new point and channel for each daemon
+            int[] partition = new int[end - start];
+            System.arraycopy(arr, start, partition, 0, end - start);
+
             point newPoint = info.createPoint();
             channel newChannel = newPoint.createChannel();
 
             channels.add(newChannel);
 
-            // Pass the partitioned array to each daemon
-            int[] partition = new int[end - start];
-            System.arraycopy(arr, start, partition, 0, end - start);
-
-            // Send partition to the daemon for sorting
             newChannel.write(partition);
+            newChannel.write(end - start);
 
-            // Execute the BubbleSort program on the daemon
             newPoint.execute("BubbleSort.jar");
         }
 
-        // Collect sorted partitions from daemons
         List<Integer> sortedList = new ArrayList<>();
         for (channel channel : channels) {
             int[] sortedPartition = (int[]) channel.readObject();
@@ -60,14 +56,12 @@ public class Main {
             }
         }
 
-        // Merge sorted partitions
         int[] sortedArray = new int[n];
         for (int i = 0; i < n; i++) {
             sortedArray[i] = sortedList.get(i);
         }
 
-        // Perform the final bubble sort step on the sorted partitions
-        bubbleSort(sortedArray);
+        mergeSort(sortedArray);
 
         System.out.println("Sorted array:");
         for (int num : sortedArray) {
@@ -81,10 +75,62 @@ public class Main {
         curtask.end();
     }
 
-    private static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+    private static void mergeSort(int[] arr) {
+        if (arr.length > 1) {
+            int mid = arr.length / 2;
+            int[] left = new int[mid];
+            int[] right = new int[arr.length - mid];
+
+            System.arraycopy(arr, 0, left, 0, mid);
+            System.arraycopy(arr, mid, right, 0, arr.length - mid);
+
+            mergeSort(left);
+            mergeSort(right);
+
+            merge(arr, left, right);
+        }
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < left.length) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < right.length) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    public void run(AMInfo info) {
+        channel channel = info.parent.readChannel();
+        int[] partition = (int[]) channel.readObject();
+        int length = channel.readInt();
+
+        bubbleSort(partition, length);
+
+        channel.write(partition);
+    }
+
+    private void bubbleSort(int[] arr, int length) {
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
