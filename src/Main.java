f0497250b4b699e72f078299f25839c8e1388a5c
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-25 12:22:23 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -96,7 +96,7 @@ public class Main implements AM {
 
         long[] sortedArray = new long[array.length];
         for (int i = 0; i < numWorkers; i++) {
-            long[] workerResult = (long[]) channels.get(i).readObject();
+            int[] workerResult = (int[]) channels.get(i).readObject();
             System.arraycopy(workerResult, 0, sortedArray, i * chunkSize, workerResult.length);
         }
 
