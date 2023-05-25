064ab332803e27731aae165b9af8e64fa99d7d99
scandiy <38243615+scandiy@users.noreply.github.com>
2023-05-25 11:12:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         Scanner sc = new Scanner(new File(curtask.findFile("input")));
         int n = sc.nextInt();
         int[] arr = new int[n];
-        
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
@@ -24,18 +24,51 @@ public class Main {
 
         List<channel> channels = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+        // Divide the array into equal parts
+        int numDaemons = info.cores;
+        int partitionSize = n / numDaemons;
+
+        for (int i = 0; i < numDaemons; i++) {
+            int start = i * partitionSize;
+            int end = (i == numDaemons - 1) ? n : (i + 1) * partitionSize;
+
+            // Create a new point and channel for each daemon
+            point newPoint = info.createPoint();
+            channel newChannel = newPoint.createChannel();
+
+            channels.add(newChannel);
+
+            // Pass the partitioned array to each daemon
+            int[] partition = new int[end - start];
+            System.arraycopy(arr, start, partition, 0, end - start);
+
+            // Send partition to the daemon for sorting
+            newChannel.write(partition);
+
+            // Execute the BubbleSort program on the daemon
+            newPoint.execute("BubbleSort.jar");
+        }
+
+        // Collect sorted partitions from daemons
+        List<Integer> sortedList = new ArrayList<>();
+        for (channel channel : channels) {
+            int[] sortedPartition = (int[]) channel.readObject();
+            for (int num : sortedPartition) {
+                sortedList.add(num);
             }
         }
 
+        // Merge sorted partitions
+        int[] sortedArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            sortedArray[i] = sortedList.get(i);
+        }
+
+        // Perform the final merge sort step on the sorted partitions
+        mergeSort(sortedArray);
+
         System.out.println("Sorted array:");
-        for (int num : arr) {
+        for (int num : sortedArray) {
             System.out.print(num + " ");
         }
         System.out.println();
@@ -44,5 +77,48 @@ public class Main {
         System.out.println("Execution time: " + totalTime + " seconds");
 
         curtask.end();
+    }
+
+    private static void mergeSort(int[] arr) {
+        if (arr.length > 1) {
+            int mid = arr.length / 2;
+            int[] left = new int[mid];
+            int[] right = new int[arr.length - mid];
+
+            System.arraycopy(arr, 0, left, 0, mid);
+            System.arraycopy(arr, mid, right, 0, arr.length - mid);
+
+            mergeSort(left);
+            mergeSort(right);
+
+            merge(arr, left, right);
+        }
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < left.length) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < right.length) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
     }
 }
